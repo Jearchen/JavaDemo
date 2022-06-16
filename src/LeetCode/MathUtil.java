157cba2e643cfa9895066e6e8b12cf607b335769
JC-Chen <2956306332@qq.com>
2022-06-17 00:59:33 +0800

    2022-6-17-二分法刷题

---
+++ src/LeetCode/MathUtil.java
@@ -0,0 +1,27 @@
+package LeetCode;
+
+public class MathUtil {
+
+    /**
+     * 计算给定数值阶乘
+     * @param n
+     * @return
+     */
+    public static int factoryN(int n){
+        int result = 1;
+        for (int i = n; i >0; i--) {
+            result = result*i;
+        }
+        return  result;
+    }
+
+    /**
+     * 计算给定组合数的Cn,m
+     * @param n
+     * @param m
+     * @return
+     */
+    public static int Cnm(int n,int m){
+        return factoryN(n) / (factoryN(m)*factoryN(n-m));
+    }
+}
